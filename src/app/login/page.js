61fe9a2fdf86facb5c5058566161c17149b34ecf
luciannomas <luciano.mastran@gmail.com>
2023-11-15 23:30:17 -0300

    second commit

---
+++ src/app/login/page.js
@@ -0,0 +1,66 @@
+"use client";
+import { FormEvent, useState } from "react";
+import { AxiosError } from "axios";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+
+function Signin() {
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const { data: session, status } = useSession();
+  console.log("hola", session);
+
+    if(session){
+        return router.push('/dashboard/profile');
+    }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const res = await signIn("credentials", {
+      email: formData.get("email"),
+      password: formData.get("password"),
+      redirect: false,
+    });
+
+    if (res?.error) setError(res.error);
+
+    if (res?.ok) return router.push("/dashboard/profile");
+  };
+
+  return (
+    <div className="justify-center h-[calc(100vh-4rem)] flex items-center">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-neutral-950 px-8 py-10 w-3/12"
+      >
+        {error && <div className="bg-red-500 text-white p-2 mb-2">{error}</div>}
+        <h1 className="text-4xl font-bold mb-7">Signin</h1>
+
+        <label className="text-slate-300">Email:</label>
+        <input
+          type="email"
+          placeholder="Email"
+          className="bg-zinc-800 px-4 py-2 block mb-2 w-full"
+          name="email"
+        />
+
+        <label className="text-slate-300">Password:</label>
+        <input
+          type="password"
+          placeholder="Password"
+          className="bg-zinc-800 px-4 py-2 block mb-2 w-full"
+          name="password"
+        />
+
+        <button className="bg-blue-500 text-white px-4 py-2 block w-full mt-4">
+          Signup
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Signin;
